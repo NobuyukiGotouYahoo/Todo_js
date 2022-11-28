2e0806c919da09a17463a436675a63012ca2194c
NobuyukiGotouYahoo <119094014+NobuyukiGotouYahoo@users.noreply.github.com>
2022-11-28 11:59:23 +0900

    追加要素生成一応完了

--- src/index.js
+++ src/index.js
@@ -9,16 +9,20 @@ const onClickAdd = () => {
 
   // li要素を生成する
   const incompliteLi = document.createElement("li");
+
   // div class="list-row"を生成
   const divListRow = document.createElement("div");
-  divListRow.setAttribute("class", "list-row");
+  divListRow.className = "list-row";
+
   //<p class="title">TEXTの入力内容</p>
   const pInputText = document.createElement("p");
   pInputText.innerText = inputText;
   divListRow.appendChild(pInputText);
+
   //<div class="action-buttons">
   const divActionButtons = document.createElement("div");
-  divActionButtons.setAttribute("class", "action-buttons");
+  divActionButtons.className = "action-buttons";
+
   //<button>完了</button><button>削除</button>
   const buttunComplete = document.createElement("button");
   buttunComplete.innerText = "完了";
@@ -27,6 +31,7 @@ const onClickAdd = () => {
   buttunDelete.innerText = "削除";
   divActionButtons.appendChild(buttunDelete);
 
+  // 生成したdiv要素を、親に追加する
   divListRow.appendChild(divActionButtons);
   incompliteLi.appendChild(divListRow);
   console.log(incompliteLi);
